7fdbe005a7a35b70698daef7665eaff0d7333f0c
chunjie <chunjie@vip.126.com>
2023-06-28 22:07:59 +0800

    暂存

--- bidding-manger/src/main/java/cn/sx/ebj/bidding/MangerApplication.java
+++ bidding-manger/src/main/java/cn/sx/ebj/bidding/MangerApplication.java
@@ -3,10 +3,8 @@ package cn.sx.ebj.bidding;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableRedisHttpSession
 @MapperScan("cn.sx.ebj.bidding.data.dao")
 public class MangerApplication {
     public static void main(String[] args) {
