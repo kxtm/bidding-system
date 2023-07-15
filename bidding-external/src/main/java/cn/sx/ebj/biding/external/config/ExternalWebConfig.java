1836f926cd3dd08c43e1100f12f30ee30b490021
chunjie <chunjie@vip.126.com>
2023-07-15 12:22:45 +0800

    增加外置扩展模块

---
+++ bidding-external/src/main/java/cn/sx/ebj/biding/external/config/ExternalWebConfig.java
@@ -0,0 +1,14 @@
+package cn.sx.ebj.biding.external.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @name: ExternalConfig
+ * @author: chunjie
+ * @date: 2023-07-15 12:20
+ **/
+@Configuration
+public class ExternalWebConfig implements WebMvcConfigurer {
+
+}
