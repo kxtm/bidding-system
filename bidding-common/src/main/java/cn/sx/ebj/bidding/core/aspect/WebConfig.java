7d724f3a5117b55c3e8f04cf2625b5978265af32
chunjie <chunjie@vip.126.com>
2023-07-18 19:28:47 +0800

    整理模块

--- bidding-common/src/main/java/cn/sx/ebj/bidding/core/aspect/WebConfig.java
+++ bidding-common/src/main/java/cn/sx/ebj/bidding/core/aspect/WebConfig.java
@@ -1,12 +1,16 @@
-package cn.sx.ebj.bidding.config;
+package cn.sx.ebj.bidding.core.aspect;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * @name: WebConfig
+ * @author: chunjie
+ * @date: 2023-07-18 19:27
+ **/
 @Configuration
-public class SupplierWebConfig implements WebMvcConfigurer {
-
+public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
