e0c29ca37568f1afb14d85fb189852fbd940315b
chunjie <chunjie@vip.126.com>
2023-07-18 19:10:30 +0800

    增加定时任务模块

---
+++ bidding-job/src/main/java/cn/sx/ebj/bidding/JobApplication.java
@@ -0,0 +1,17 @@
+package cn.sx.ebj.bidding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @name: JobApplication
+ * @author: chunjie
+ * @date: 2023-07-18 19:09
+ **/
+@SpringBootApplication
+public class JobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobApplication.class,args);
+    }
+}
