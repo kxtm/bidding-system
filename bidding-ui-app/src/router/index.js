389a2ad844451d9d25b3cf5769e8bfa8ef47458d
chunjie <chunjie@vip.126.com>
2023-10-23 14:03:02 +0800

    增肌登录入口

--- bidding-ui-app/src/router/index.js
+++ bidding-ui-app/src/router/index.js
@@ -1,33 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {createRouter, createWebHistory} from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path:'/',
-      component:()=>import('@/views/HomeView.vue')
-    },
-    {
-      path:'/login',
-      name:'login',
-      component:()=>import('@/views/LoginView.vue')
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            component: () => import('@/views/HomeView.vue'),
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('@/views/LoginView.vue')
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import('../views/AboutView.vue')
+        }
+    ]
 })
 
 router.beforeEach((to, from, next) => {
-  // if (to.path !== "/login") {
-  //   router.push("/login").then(r =>{});
-  // }
-  next()
+    if (to.path !== "/login") {
+        router.push("/login").then(r => {
+          console.log(from.fullPath)
+        });
+    } else {
+        next()
+    }
+
 })
 export default router
--- bidding-ui-app/src/views/LoginView.vue
+++ bidding-ui-app/src/views/LoginView.vue
@@ -1,11 +1,25 @@
 <template>
-  <div >
-      <div class="input-group">
-         <input type="text" class="form-control" />
+  <div class="login-container">
+    <div class="panel panel-default login-panel">
+      <div class="panel-heading">登录入口</div>
+      <div class="panel-body">
+        <div class="input-group">
+          <span class="input-group-addon glyphicon glyphicon-user"></span>
+          <input type="text" class="form-control" placeholder="用户名">
+        </div>
+        <div class="input-group">
+          <span class="input-group-addon glyphicon glyphicon-lock" ></span>
+          <input type="password" class="form-control" placeholder="密码">
+        </div>
+        <div class="input-group">
+          <span class="input-group-addon glyphicon glyphicon-picture"></span>
+          <input type="text" class="form-control" placeholder="验证码">
+        </div>
+        <div class="btn-group">
+           <button class="btn btn-info " id="login-btn" type="button">登录</button>
+        </div>
       </div>
+
+    </div>
   </div>
 </template>
-
-<style>
-
-</style>
