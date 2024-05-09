993f75ed6ae9fb19d03838f4e7fcd90b3f23792b
chunjie <wcj@ebj.sx.cn>
2024-05-09 15:39:41 +0800

    element plus 测试

--- bidding-ui-app/src/views/LoginView.vue
+++ bidding-ui-app/src/views/LoginView.vue
@@ -1,23 +1,39 @@
 <template>
-  <div class="mb-4">
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-  </div>
 
-  <div>
-    <el-button :icon="Search" circle />
-    <el-button type="primary" :icon="Edit" circle />
-    <el-button type="success" :icon="Check" circle />
-    <el-button type="info" :icon="Message" circle />
-    <el-button type="warning" :icon="Star" circle />
-    <el-button type="danger" :icon="Delete" circle />
-
-  </div>
+  <el-form ref="loginForm" :model="form" label-width="80px">
+    <el-form-item label="账号">
+      <el-input v-model="form.username" autocomplete="off" style="width: 240px" placeholder="用户名"></el-input>
+    </el-form-item>
+    <el-form-item label="密码">
+      <el-input type="password" v-model="form.password" style="width: 240px" autocomplete="off"
+                placeholder="密码"></el-input>
+    </el-form-item>
+    <el-form-item label="验证码">
+      <el-input v-model="form.code" style="width: 240px" autocomplete="off" placeholder="验证码"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm"  >
+        登录
+      </el-button>
+      <el-button type="primary" style="margin-left: 16px" @click="drawer = true">
+        with footer
+      </el-button>
+    </el-form-item>
+  </el-form>
+  <RightDrawer v-model="drawer" />
 </template>
-<script setup>
-import {Check, Delete, Edit, Message, Search, Star} from '@element-plus/icons-vue'
+<script lang="ts" setup>
+import {reactive,ref} from 'vue'
+import RightDrawer from "@/components/RightDrawer.vue";
+const drawer=ref(false)
+// do not use same name with ref
+const form = reactive({
+  username: '',
+  password: '',
+  code: ''
+});
+
+const submitForm = () => {
+
+}
 </script>
