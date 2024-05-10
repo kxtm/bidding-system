3558eff96214ebd2267001af3a44099e7df7bbf6
chunjie <wcj@ebj.sx.cn>
2024-05-10 16:53:14 +0800

    扩展方法

--- bidding-core/src/main/java/com/chunjies/bidding/common/core/Result.java
+++ bidding-core/src/main/java/com/chunjies/bidding/common/core/Result.java
@@ -14,7 +14,7 @@ import java.io.Serializable;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Result<T> implements Serializable {
     //成功 true 失败 false
-    private boolean code;
+    private Boolean code;
     //消息
     private String msg;
     //数据
@@ -38,6 +38,16 @@ public class Result<T> implements Serializable {
     public static <T> Result<T> ok(String msg, T data) {
 
         return new Result<>(true, msg, data);
+    }
+
+    /**
+     * @param data
+     * @param <T>
+     * @return
+     */
+    public static <T> Result<T> ok(T data) {
+
+        return new Result<>(true, SUCCESS_DEFAULT_MSG, data);
     }
 
     /**
@@ -76,11 +86,15 @@ public class Result<T> implements Serializable {
         return new Result<>(false, msg, null);
     }
 
-    public boolean isCode() {
+    public static <T> Result<T> error() {
+        return new Result<>(false, ERROR_DEFAULT_MSG, null);
+    }
+
+    public Boolean isCode() {
         return code;
     }
 
-    public void setCode(boolean code) {
+    public void setCode(Boolean code) {
         this.code = code;
     }
 
@@ -99,4 +113,8 @@ public class Result<T> implements Serializable {
     public void setData(T data) {
         this.data = data;
     }
+
+
+    final static String ERROR_DEFAULT_MSG = "处理错误";
+    final static String SUCCESS_DEFAULT_MSG = "处理成功";
 }
