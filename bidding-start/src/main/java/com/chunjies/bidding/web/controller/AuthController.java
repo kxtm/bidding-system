8575d36fdfaf381e2f000eb474a9452e616005e2
chunjie <wcj@ebj.sx.cn>
2024-05-10 16:35:45 +0800

    暂存

--- bidding-start/src/main/java/com/chunjies/bidding/web/controller/AuthController.java
+++ bidding-start/src/main/java/com/chunjies/bidding/web/controller/AuthController.java
@@ -1,21 +1,22 @@
 package com.chunjies.bidding.web.controller;
 
 import com.chunjies.bidding.common.core.IController;
+import com.chunjies.bidding.common.core.Result;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @name: MangerController
- * @author: chunjie
- * @date: 2022-11-19 21:21
+ * {@code @name:} MangerController
+ * {@code @author:} chunjie
+ * {@code @date:} 2022-11-19 21:21
  **/
 @RestController
 @RequestMapping("/auth")
-public class LoginController extends IController {
+public class AuthController extends IController {
 
     @GetMapping("/")
-    public String access(){
-        return "manger/login";
+    public Result<String> access() {
+        return Result.error("请求错误");
     }
 }
